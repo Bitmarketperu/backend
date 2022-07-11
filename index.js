f2ef7ccbd1a836cc1b3fb7014d37a54a33794be6
Jason Hernandez <kaltre10@gmail.com>
2022-07-11 18:20:58 -0500

    init express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('ok')
+const express = require('express');
+const router = express.Router();
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('BitmarketPeru');
+})
+
+const port = process.env.PORT || 4000;
+
+app.listen( port, () => console.log(`App listening on port, ${port}`));
