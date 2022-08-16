a8fd0f69070586055d88e4766d273732227940a1
Jason Hernandez <kaltre10@gmail.com>
2022-08-15 20:47:30 -0500

    transaction admin

--- components/transactions/store.js
+++ components/transactions/store.js
@@ -3,9 +3,9 @@ const Model = require('./model');
 const addtransaction = transaction => Model(transaction).save();
 const getTransaction = _id => Model.find({ _id }).populate('bank');
 
-const getUserTransaction = (wallet, desde, hasta) => Model.find({ wallet, date: {"$gte": desde, "$lt": hasta} }).populate('bank');
+const getUserTransaction = (wallet, desde, hasta) => Model.find({ wallet, date: {"$gte": desde, "$lt": hasta} }).populate('bank').populate('bankAdmin');
 
-const getAllTransactions = (desde, hasta) => Model.find({date: {"$gte": desde, "$lt": hasta}}).populate('bank');
+const getAllTransactions = (desde, hasta) => Model.find({date: {"$gte": desde, "$lt": hasta}}).populate('bank').populate('bankAdmin');
 
 const setTransaction = (_id, status) => Model.findOneAndUpdate({ _id }, { status }, { new: true})
 
