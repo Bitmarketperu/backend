bbb9b0c838c8b3cc2a8abe75e8d165ddb502b7b8
Jason Hernandez <kaltre10@gmail.com>
2022-08-16 08:20:47 -0500

    update time utc to peru

--- components/transactions/controller.js
+++ components/transactions/controller.js
@@ -5,8 +5,9 @@ const getUserTransaction = ( wallet, desde, hasta ) => {
     return new Promise( async (resolve, reject) => {
         try {
             
-            desde = desde + "T00:00:00.000+00:00";
-            hasta = hasta + "T23:59:59.000+00:00";
+            desde = desde + "T05:00:00.000+00:00";
+            hasta = hasta + "T04:59:59.000+00:00";
+
             const transactions = await store.getAllUser(wallet, desde, hasta);
      
             resolve(transactions);
@@ -23,8 +24,8 @@ const getALLTransaction = ( desde, hasta ) => {
     return new Promise( async (resolve, reject) => {
         try {
 
-            desde = desde + "T00:00:00.000+00:00";
-            hasta = hasta + "T23:59:59.000+00:00";
+            desde = desde + "T05:00:00.000+00:00";
+            hasta = hasta + "T04:59:59.000+00:00";
             const transactions = await store.getAll(desde, hasta);
      
             resolve(transactions);
