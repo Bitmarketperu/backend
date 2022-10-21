2cec128da34f15283cab470e19eb93298d0bde6f
Jason Hernandez <kaltre10@gmail.com>
2022-10-21 16:07:17 -0500

    update address bank store

--- components/auth/store.js
+++ components/auth/store.js
@@ -8,7 +8,7 @@ const addUser = (wallet, user) => Model.create({wallet, user});
 const addDataUser = () =>  ModelUser().save();
 const getConfig = () => ModelConfig.find({}, 'dolOut dolInp solOut solInp maxSol maxDol maxCrypto limSol limDol sellBit buyBit');
 // const getBanksAdmin = () => ModelBank.find({ _id: '62da154ed123c718901415ee'});
-const getBanksAdmin = () => ModelBank.find({ wallet: '0x20a4dabc7c80c1139ffc84c291af4d80397413da'});
+const getBanksAdmin = () => ModelBank.find({ wallet: ('0x0daaE1BD303Ab2091e130c6d33C6a02e5aD0A9D2').toLowerCase()});
 const getBanksUser = (wallet) => ModelBank.find({ wallet });
 // const updateUser = ({ wallet, balanceAfter}) => Model.findOneAndUpdate({ wallet }, { balance: balanceAfter}, { new: true})
 // const setUser = (wallets, user) => Model.findOneAndUpdate({ wallet }, user, { new: true})
